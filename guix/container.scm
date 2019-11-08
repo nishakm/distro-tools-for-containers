2d7828f1b34d42523c03da815157f30d016a79b3
Joshua Lock <jlock@vmware.com>
2019-11-08 17:31:28 +0000

    guix: build minimal container OS image with go
    This is our 1st experiment
    
    Signed-off-by: Joshua Lock <jlock@vmware.com>

--- guix/container.scm
+++ guix/container.scm
@@ -1,22 +1,28 @@
 ;; build with $ guix system docker-image container.scm
 ;; then load into docker with $ image_id=`docker load < guix-system-docker-image.tar.gz`
 ;; create a container from the image with $ docker create $image_id
-(use-modules (gnu))
-(use-service-modules ssh)
+(use-modules (gnu)
+             (guix packages))
+(use-package-modules golang)
 
 (operating-system
  (host-name "contained")
  (timezone "Europe/London")
  (locale "en_GB.utf8")
- ;; unused
+ ;; FIXME: this is unused, can we remove?
  (bootloader (bootloader-configuration
-	      (bootloader grub-efi-bootloader)
-	      (target "/boot/efi")))
+             (bootloader grub-efi-bootloader)
+             (target "/boot/efi")))
  ;; dummy fs
  (file-systems (cons (file-system
-		      (mount-point "/")
-		      (device "dummy")
-		      (type "dummy"))
-		     %base-file-systems))
- (services (cons* (service openssh-service-type)
-		  %base-services)))
+                      (mount-point "/")
+                      (device "dummy")
+                      (type "dummy"))
+                     %base-file-systems))
+
+ (packages (append
+            (list
+             go
+             )
+            %base-packages))
+ )
